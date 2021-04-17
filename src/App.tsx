8003fd3adb83c23f8dee373233062462bdaf2800
Han Wang <hyw2@cornell.edu>
2021-04-16 21:08:57 -0400

    Set up header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 function App() {
   return (
-    <div className="background">
-      <div className="header">Email Demo</div>
+    <div className="p-2 bg-transparent w-full h-full">
+      <div className="shadow-lg">
+        <div className="px-6 py-4 bg-gray-700 w-full text-white font-bold rounded-t-lg">
+          Email Demo
+        </div>
+        <div className="bg-white px-6 py-6">
+          <h1 className="text-sm">
+            <a className="text-gray-500">To: </a>
+            <a className="font-semibold text-gray-800">
+              Hooli Customer Support (support@hooli.xyz)
+            </a>
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
