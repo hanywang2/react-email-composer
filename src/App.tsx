d59a957a1859851c552ca8c081e92b37db35b8b8
Han Wang <hyw2@cornell.edu>
2021-04-25 18:08:50 -0400

    Add responsiveness to outer padding

--- src/App.tsx
+++ src/App.tsx
@@ -177,7 +177,7 @@ function App() {
     editorState.getCurrentContent().hasText() && isEmailValid;
 
   return (
-    <div className="p-8 bg-transparent w-full h-screen">
+    <div className="sm:p-8 bg-transparent w-full h-screen">
       <div className="flex shadow-xl flex-col h-full bg-white rounded-lg text-lg">
         <div
           className="px-6 py-4 w-full text-white font-bold rounded-t-lg"
